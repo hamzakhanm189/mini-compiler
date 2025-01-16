fedcbc8599182dd89932eea01af4288871a0896f
hamzakhanm189 <hamzakhanm189@gmail.com>
2025-01-16 18:10:17 +0500

    Add files via upload
    Lab 3

---
+++ Lab 3/Lab 2 Task 1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lab_2_Task_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "if (a && b || !c)";
+            string pattern = @"(\&\&|\|\||!)";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine("Input String: if (a && b || !c)");
+            Console.WriteLine("\nOutput: (Regular expression for logical operators):");
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Found logical operator: {match.Value}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
